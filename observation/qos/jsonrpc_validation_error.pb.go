194671d2f90ecc26d4382e44175414799d500daf
Otto V <ottoevargas@gmail.com>
2025-12-04 10:38:27 +0100

    run make proto_regen

--- observation/qos/jsonrpc_validation_error.pb.go
+++ observation/qos/jsonrpc_validation_error.pb.go
@@ -14,13 +14,12 @@
 package qos
 
 import (
-	reflect "reflect"
-	sync "sync"
-	unsafe "unsafe"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
 )
 
 const (
