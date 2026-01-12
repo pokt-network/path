5993d34edaf67585e84acd10c4c92a0386f30a21
Otto V <ottoevargas@gmail.com>
2026-01-12 18:28:29 +0100

    Add gRPC host and security config to Shannon full node settings for e2e

--- e2e/scripts/ci/update_shannon_config_from_secrets.sh
+++ e2e/scripts/ci/update_shannon_config_from_secrets.sh
@@ -30,6 +30,8 @@ update_shannon_config_from_env() {
 	.gateway_config.gateway_address = env(SHANNON_GATEWAY_ADDRESS) |
 	.gateway_config.gateway_private_key_hex = env(SHANNON_GATEWAY_PRIVATE_KEY) |
 	.full_node_config.rpc_url = env(FULL_NODE_RPC_URL) |
+  .full_node_config.grpc_config.host_port = "sauron-grpc.infra.pocket.network:443" |
+  .full_node_config.grpc_config.insecure = false |
 	.gateway_config.owned_apps_private_keys_hex = (env(SHANNON_OWNED_APPS_PRIVATE_KEYS) | split(","))
     ' $CONFIG_FILE
 }
